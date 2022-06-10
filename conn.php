7ee400a3da26f92e8ae43e473740ee33bb40c918
Tejus <tejusgroup20@gmail.com>
2022-06-10 19:57:32 +0530

    initial commit

---
+++ conn.php
@@ -0,0 +1,34 @@
+<?php
+if(isset($_POST['name'])){
+$server="localhost";
+$username="root";
+$password="";
+
+$con=mysqli_connect($server, $username, $password);
+
+if(!$con){
+    die("connection to this database failed due to" . mysqli_connect_error());
+}
+// echo "Success Connecting to the db"
+
+$name=$_POST['name'];
+$gender=$_POST['gender'];
+$age=$_POST['age'];
+$email=$_POST['email'];
+$phone=$_POST['phone'];
+$desc=$_POST['desc'];
+
+$sql="INSERT INTO `trip` (`name`, `age`, `gender`, `email`, `phone`, `other`, `dt`) VALUES ('$name', '$age', '$gender', '$email', '$phone', '$desc', current_timestamp());";
+
+echo $sql;
+
+if($con->query($sql)==true){
+    echo "Successfully inserted";
+}
+else{
+    echo "ERROR: $sql <br> $con->error";
+}
+
+$con->close();
+}
+?>
